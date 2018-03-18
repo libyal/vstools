ca5d48f1698c4715483c0c72022f4a6608ebc2ef
Joachim Metz <joachim.metz@gmail.com>
2018-03-18 07:42:20 +0100

    Updated CI test configuration and helper scripts

--- utils/check_dependencies.py
+++ utils/check_dependencies.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Script to check for the availability and version of dependencies."""
 
 import sys
 
 # Change PYTHONPATH to include dependencies.
-sys.path.insert(0, u'.')
+sys.path.insert(0, '.')
 
 import utils.dependencies  # pylint: disable=wrong-import-position
 
 
-if __name__ == u'__main__':
+if __name__ == '__main__':
   dependency_helper = utils.dependencies.DependencyHelper()
 
   dependency_helper.CheckDependencies()

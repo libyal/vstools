50e3eb818e2f4c43fa8a7ce82210bd344c575ae5
Joachim Metz <joachim.metz@gmail.com>
2019-05-19 09:11:03 +0200

    Moved coverage CI tests to Ubuntu 18.04 on Docker

--- config/travis/runtests.sh
+++ config/travis/runtests.sh
@@ -8,22 +8,7 @@
 # Exit on error.
 set -e;
 
-if test "${TARGET}" = "jenkins";
-then
-	./config/jenkins/linux/run_end_to_end_tests.sh "travis";
-
-elif test "${TARGET}" = "pylint";
-then
-	pylint --version
-
-	for FILE in `find setup.py config scripts tests vstools -name \*.py`;
-	do
-		echo "Checking: ${FILE}";
-
-		pylint --rcfile=.pylintrc ${FILE};
-	done
-
-elif test "${TRAVIS_OS_NAME}" = "osx";
+if test "${TRAVIS_OS_NAME}" = "osx";
 then
 	PYTHONPATH=/Library/Python/2.7/site-packages/ /usr/bin/python ./run_tests.py;
 
@@ -38,43 +23,67 @@ then
 		PYTHONPATH=. python ./tests/end-to-end.py --debug -c config/end-to-end.ini;
 	fi
 
-elif test "${TRAVIS_OS_NAME}" = "linux";
+elif test -n "${FEDORA_VERSION}";
 then
-	COVERAGE="/usr/bin/coverage";
-
-	if ! test -x "${COVERAGE}";
-	then
-		# Ubuntu has renamed coverage.
-		COVERAGE="/usr/bin/python-coverage";
-	fi
+	CONTAINER_NAME="fedora${FEDORA_VERSION}";
+	CONTAINER_OPTIONS="-e LANG=en_US.UTF-8";
 
 	if test -n "${TOXENV}";
 	then
-		tox --sitepackages ${TOXENV};
+		TEST_COMMAND="tox -e ${TOXENV}";
 
-	elif test "${TRAVIS_PYTHON_VERSION}" = "2.7";
+	elif test "${TARGET}" = "pylint";
 	then
-		${COVERAGE} erase
-		${COVERAGE} run --source=vstools --omit="*_test*,*__init__*,*test_lib*" ./run_tests.py
+		TEST_COMMAND="./config/travis/run_pylint.sh";
+
+	elif test ${TRAVIS_PYTHON_VERSION} = "2.7";
+	then
+		TEST_COMMAND="./config/travis/run_python2.sh";
 	else
-		python ./run_tests.py
-
-		python ./setup.py build
-
-		python ./setup.py sdist
-
-		python ./setup.py bdist
-
-		TMPDIR="${PWD}/tmp";
-		TMPSITEPACKAGES="${TMPDIR}/lib/python${TRAVIS_PYTHON_VERSION}/site-packages";
-
-		mkdir -p ${TMPSITEPACKAGES};
-
-		PYTHONPATH=${TMPSITEPACKAGES} python ./setup.py install --prefix=${TMPDIR};
-
-		if test -f tests/end-to-end.py;
-		then
-			PYTHONPATH=. python ./tests/end-to-end.py --debug -c config/end-to-end.ini;
-		fi
+		TEST_COMMAND="./config/travis/run_python3.sh";
 	fi
+	# Note that exec options need to be defined before the container name.
+	docker exec ${CONTAINER_OPTIONS} ${CONTAINER_NAME} sh -c "cd vstools && ${TEST_COMMAND}";
+
+elif test -n "${UBUNTU_VERSION}";
+then
+	CONTAINER_NAME="ubuntu${UBUNTU_VERSION}";
+	CONTAINER_OPTIONS="-e LANG=en_US.UTF-8";
+
+	if test -n "${TOXENV}";
+	then
+		TEST_COMMAND="tox -e ${TOXENV}";
+
+	elif test "${TARGET}" = "coverage";
+	then
+		# Also see: https://docs.codecov.io/docs/testing-with-docker
+		curl -o codecov_env.sh -s https://codecov.io/env;
+
+		# Generates a series of -e options.
+		CODECOV_ENV=$(/bin/bash ./codecov_env.sh);
+
+		CONTAINER_OPTIONS="${CODECOV_ENV} ${CONTAINER_OPTIONS}";
+
+		TEST_COMMAND="./config/travis/run_coverage.sh";
+
+	elif test "${TARGET}" = "jenkins2";
+	then
+		TEST_COMMAND="./config/jenkins/linux/run_end_to_end_tests.sh travis";
+
+	elif test "${TARGET}" = "jenkins3";
+	then
+		TEST_COMMAND="./config/jenkins/linux/run_end_to_end_tests_py3.sh travis";
+
+	elif test "${TARGET}" = "pylint";
+	then
+		TEST_COMMAND="./config/travis/run_pylint.sh";
+
+	elif test ${TRAVIS_PYTHON_VERSION} = "2.7";
+	then
+		TEST_COMMAND="./config/travis/run_python2.sh";
+	else
+		TEST_COMMAND="./config/travis/run_python3.sh";
+	fi
+	# Note that exec options need to be defined before the container name.
+	docker exec ${CONTAINER_OPTIONS} ${CONTAINER_NAME} sh -c "cd vstools && ${TEST_COMMAND}";
 fi
